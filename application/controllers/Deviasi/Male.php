d694ca6d3ab3046b761ab00c5d75f4df560bc6f4
wahyuh1997 <wahyuh1997@gmail.com>
2022-09-02 20:45:57 +0700

    Update Rules...

--- application/controllers/Deviasi/Male.php
+++ application/controllers/Deviasi/Male.php
@@ -8,13 +8,14 @@ class Male extends MY_Controller
   public function __construct()
   {
     parent::__construct();
+    /* Load Model Here */
   }
 
-  /**
-   * index
-   */
   public function index()
   {
+    /* load Function Model Here to Show All Data*/
+    $res = '';
+
     $data = [
       'title'     => 'Standar Deviasi Berat Badan',
       'subtitle'  => 'Anak Laki-Laki',
@@ -24,10 +25,40 @@ class Male extends MY_Controller
 
   public function add()
   {
-    $data = [
-      'title'     => 'Tambah Data Standar Deviasi Berat Badan (KG)',
-      'subtitle'  => 'Anak Laki-Laki',
-    ];
-    $this->load_template('male/page/add', $data);
+    $post = $this->input->post(null, true);
+    /* If Need Selection Load Here */
+    $res = '';
+
+    if (count($post) == 0) {
+      $data = [
+        'title'     => 'Tambah Data Standar Deviasi Berat Badan (KG)',
+        'subtitle'  => 'Anak Laki-Laki',
+      ];
+      $this->load_template('male/page/add', $data);
+    } else {
+      /* Save Data Function */
+    }
+  }
+
+  public function edit($id)
+  {
+    $post = $this->input->post(null, true);
+    /* load Function Model Here to Get Data By ID*/
+    $res = '';
+
+    if (count($post) == 0) {
+      $data = [
+        'title'     => 'Tambah Data Standar Deviasi Berat Badan (KG)',
+        'subtitle'  => 'Anak Laki-Laki',
+      ];
+      $this->load_template('male/page/edit', $data);
+    } else {
+      /* Update Data Function */
+    }
+  }
+
+  public function delete($id)
+  {
+    /* Delete Data Function */
   }
 }
