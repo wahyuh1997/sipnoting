c8695409ddc5642cca52fdc09a83bb7522728763
Andirifaldi <andirifaldi31@gmail.com>
2022-09-08 21:38:10 +0700

    upd202209082138

--- application/controllers/Dashboard.php
+++ application/controllers/Dashboard.php
@@ -8,6 +8,7 @@ class Dashboard extends MY_Controller
   public function __construct()
   {
     parent::__construct();
+    $this->load->model(['Dashboard_model' => 'dashboard']);
   }
 
   /**
@@ -15,6 +16,11 @@ class Dashboard extends MY_Controller
    */
   public function index()
   {
+    /**
+     * $this->dashboard->presentase_stunting_pertahun($tahun); ->untuk tahun optional, default : tahun sekarang
+     * $this->dashboard->presentase_stunting() -> buat ngecek presentase stunting
+     * $this->dashboard->rata_z_score() -> buat mencari rata2 z_score dan total balita
+     */
     $data = [
       'title'     => 'Dashboard',
       'subtitle'  => 'Anak Laki-Laki',
